f5cbb50dfc1a11310e05c50946876bdce43295ce
Anirudh Sharma <mcxiv14@gmail.com>
2026-02-12 13:19:30 +0530

    rusty python

--- src/hough_transform/__init__.py
+++
@@ -1,6 +0,0 @@
-from .data_generator import *
-from .hough_lines import *
-
-__init__ = [
-    "count_hough_lines",
-]
--- test/test_rusty_hough.py
+++
@@ -1,4 +0,0 @@
-from  cv_hough_transform import sum_as_string
-
-def test_sum_as_string():
-    assert sum_as_string(1, 2) == "3"
